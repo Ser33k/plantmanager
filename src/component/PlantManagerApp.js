540073ef2d2075b9dca77c9d04de540d30c23c5e
mars-low <53299913+mars-low@users.noreply.github.com>
2021-03-27 19:39:28 +0100

    Change browser router to hash router to allow for router on gh-pages

--- src/component/PlantManagerApp.js
+++ src/component/PlantManagerApp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ListPlants from "./ListPlants";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import PlantComponent from "./PlantComponent";
 import PrimarySearchAppBar from "./PrimarySearchAppBar";
 
